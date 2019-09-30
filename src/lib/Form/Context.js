9257038f07b0485bc0078519c5e995d9bb509452
Hugo Mallet <hugo.mallet@53js.fr>
2019-09-30 11:18:07 +0200

    feat: new hook useFormContext

--- src/lib/Form/Context.js
+++ src/lib/Form/Context.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 const FormContext = React.createContext();
 
 export default FormContext;
+
+export const useFormContext = () => useContext(FormContext);
 
 export const withFormContext = (cb) => <FormContext.Consumer>{cb}</FormContext.Consumer>;
